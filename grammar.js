c73687dff0e2a3fa2380f58c7a387e2602aef7e4
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2025-03-07 08:50:37 +0100

    feat: improve grammar

--- grammar.js
+++ grammar.js
@@ -11,38 +11,72 @@ module.exports = grammar({
   name: "hl7",
 
   rules: {
-    message: $ => seq($.msh, repeat(seq($.field_separator, optional($.field))), $.segment_separator, repeat($.segment)),
+    // HL7 Message Structure
+    message: ($) =>
+      seq(
+        $.msh,
+        repeat($.segment), // HL7 is made of multiple segments
+      ),
 
-    msh: $ => seq(
-      $.msh_name,
-      $.msh_controls,
-    ),
- 
-    msh_name: $ => "MSH",
-    msh_controls: $ => seq(
-      $.field_separator,
-      $.component_separator,
-      $.repetition_separator,
-      $.escape_character,
-      $.subcomponent_separator,
-    ),
-    segment_name: $ => /[A-Z0-9]{3}/,
+    // MSH Segment (Message Header)
+    msh: ($) =>
+      seq(
+        "MSH",
+        $.msh_controls,
+        repeat(seq($.field_separator, optional($.field))), // Fields after MSH
+        $.segment_separator,
+      ),
 
-    field_separator: $ => "|",
-    component_separator: $ => "^",
-    repetition_separator: $ => "~",
-    escape_character: $ => "\\",
-    subcomponent_separator: $ => "&",
-    segment_separator: $ => choice("\r", "\n", "\r\n"),
+    msh_controls: ($) =>
+      seq(
+        $.field_separator,
+        $.component_separator,
+        $.repetition_separator,
+        $.escape_character,
+        $.subcomponent_separator,
+      ),
 
-    segment: $ => seq($.segment_name, repeat(seq($.field_separator, optional($.field))), $.segment_separator),
+    // Generic Segment (e.g., PID, OBR, ORC, etc.)
+    segment: ($) =>
+      seq(
+        $.segment_name,
+        repeat(seq($.field_separator, optional($.field))), // Fields in segment
+        $.segment_separator,
+      ),
 
-    field: $ => seq($.repeat, repeat(seq($.repetition_separator, $.repeat))),
-    repeat: $ => seq($.component, repeat(seq($.component_separator, $.component))),
-    component: $ => seq($.subcomponent, repeat(seq($.subcomponent_separator, $.subcomponent))),
-    subcomponent: $ => choice($.number, $.string),
+    segment_name: ($) => /[A-Z0-9]{3}/, // HL7 segments typically 3-letter uppercase codes
 
-    number: $ => /\d+/,
-    string: $ => /[^|^~\\&\r\n]*/
-  }
+    // HL7 Separators
+    field_separator: ($) => "|",
+    component_separator: ($) => "^",
+    repetition_separator: ($) => "~",
+    escape_character: ($) => "\\",
+    subcomponent_separator: ($) => "&",
+    segment_separator: ($) => choice("\r", "\n", "\r\n"), // HL7 allows different line endings
+
+    // Field Handling
+    field: ($) =>
+      seq(
+        $.repeat, // at least one 'repeat' is required
+        repeat(seq($.repetition_separator, $.repeat)),
+      ),
+
+    repeat: ($) =>
+      seq(
+        $.component,
+        repeat(seq($.component_separator, optional($.component))),
+      ),
+
+    component: ($) =>
+      seq(
+        $.subcomponent,
+        repeat(seq($.subcomponent_separator, optional($.subcomponent))),
+      ),
+
+    subcomponent: ($) => choice($.number, $.string),
+
+    // HL7 Data Types
+    number: ($) => /\d+/,
+    string: ($) => /[^|^~\\&\r\n]+/, // Avoids breaking on separators
+  },
 });
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,18 +1,32 @@
+;; Highlight HL7 segments (e.g., MSH, PID, OBR)
+(segment_name) @keyword
+
+;; Field separators (|)
 (field_separator) @punctuation.delimiter
+
+;; Component separators (^)
 (component_separator) @punctuation.delimiter
+
+;; Repetition separators (~)
 (repetition_separator) @punctuation.delimiter
-(escape_character) @punctuation.delimiter
+
+;; Subcomponent separators (&)
 (subcomponent_separator) @punctuation.delimiter
 
-(msh_name) @markup.heading.1
-(msh_controls) @punctuation.special
-(segment_name) @markup.heading.2
+;; Escape character (\)
+(escape_character) @string.escape
 
-; (segment) @string.special
-; (field) @string.special
-; (repeat) @string.special
-; (component) @string.special
-; (subcomponent) @string.special
+;; Segment separator (CR/LF)
+(segment_separator) @punctuation.special
 
-; (number) @number
-; (string) @hint
+;; Numeric values
+(number) @number
+
+;; String values
+(string) @string
+
+;; Highlight message header
+(msh_name) @function
+
+;; Highlight control characters inside MSH
+(msh_controls) @constant.builtin
